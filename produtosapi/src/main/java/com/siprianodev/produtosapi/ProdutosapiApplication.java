f40ca7e6a1436bcc3ee162c1b2c04b586b06c7f2
Anderson Sipriano <Andsipriano@gmail.com>
2025-03-28 19:51:52 -0300

    Hello World com Spring Boot, simples na aplicação

--- produtosapi/src/main/java/com/siprianodev/produtosapi/ProdutosapiApplication.java
+++ produtosapi/src/main/java/com/siprianodev/produtosapi/ProdutosapiApplication.java
@@ -2,9 +2,18 @@ package com.siprianodev.produtosapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ProdutosapiApplication {
+
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosapiApplication.class, args);
