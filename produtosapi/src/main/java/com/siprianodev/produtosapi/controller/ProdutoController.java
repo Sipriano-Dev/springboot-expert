b57c68493abd91395cb0640403abd2e86114f167
Anderson Sipriano <Andsipriano@gmail.com>
2025-03-30 21:33:51 -0300

    Realizando busca com parametros

--- produtosapi/src/main/java/com/siprianodev/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/com/siprianodev/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import com.siprianodev.produtosapi.model.Produto;
 import com.siprianodev.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -42,6 +43,11 @@ public class ProdutoController {
         produto.setId(id);
         produtoRepository.save(produto);
 
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome) {
+        return produtoRepository.findByNome(nome);
     }
 
 }
