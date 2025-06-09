1883c680abdb4d0b8f9888e51410bc32f3f78366
Anderson Sipriano <adsspnp@gmail.com>
2025-06-09 12:59:51 -0300

    Codificando as camadas da arquitetura completa

---
+++ arquiteturaspring/src/main/java/com/sipriano/arquiteturaspring/todos/TodoController.java
@@ -0,0 +1,24 @@
+package com.sipriano.arquiteturaspring.todos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("todos")
+public class TodoController {
+
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public TodoEntity salvar(@RequestBody TodoEntity novoTodo) {
+        return todoService.salvar(novoTodo);
+    }
+
+}
---
+++ arquiteturaspring/src/main/java/com/sipriano/arquiteturaspring/todos/TodoService.java
@@ -0,0 +1,18 @@
+package com.sipriano.arquiteturaspring.todos;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public TodoEntity salvar(TodoEntity novoTodo) {
+        return todoRepository.save(novoTodo);
+    }
+
+}
