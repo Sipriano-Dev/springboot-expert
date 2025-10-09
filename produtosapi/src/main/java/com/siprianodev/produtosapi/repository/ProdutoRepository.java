e600b5fc81f8717b6b05bc42e7bf79daa6fff536
Anderson Sipriano <adsspns@gmail.com>
2025-10-09 18:13:46 -0300

    Como configurar um DataSource e um Pool de conexões

--- produtosapi/src/main/java/com/siprianodev/produtosapi/repository/ProdutoRepository.java
+++ produtosapi/src/main/java/com/siprianodev/produtosapi/repository/ProdutoRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ProdutoRepository extends JpaRepository<Produto, String> {
+public interface  ProdutoRepository extends JpaRepository<Produto, String> {
 
     List<Produto> findByNome(String nome);
 
