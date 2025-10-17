227ddf5d9475650b2a11d8b96518984ccff1e35f
Anderson Sipriano <adsspns@gmail.com>
2025-10-17 14:07:59 -0300

    Executando uma operação de persistencia com Repositorio

--- libraryapi/src/main/java/com/sipriano/libraryapi/Application.java
+++ libraryapi/src/main/java/com/sipriano/libraryapi/Application.java
@@ -1,13 +1,31 @@
 package com.sipriano.libraryapi;
 
+import com.sipriano.libraryapi.model.Autor;
+import com.sipriano.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		var context = SpringApplication.run(Application.class, args);
+		AutorRepository autorRepository = context.getBean(AutorRepository.class);
+
+		exemploSalvarRegistro(autorRepository);
+	}
+
+	public static void exemploSalvarRegistro(AutorRepository autorRepository) {
+
+		Autor autor = new Autor();
+		autor.setNome("José");
+		autor.setNacionalidade("Brasileira");
+		autor.setDataNascimento(LocalDate.of(1988, 10, 14));
+
+		var autorSalvo = autorRepository.save(autor);
+		System.out.println("Autor salvo: " + autorSalvo);
 	}
 
 }
