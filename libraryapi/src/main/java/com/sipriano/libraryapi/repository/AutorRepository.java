8f91920c09254a0ccac453dcd0be94b9146ddd26
Anderson Sipriano <adsspns@gmail.com>
2025-10-17 13:48:09 -0300

    Apresentando os Repositories

---
+++ libraryapi/src/main/java/com/sipriano/libraryapi/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package com.sipriano.libraryapi.repository;
+
+import com.sipriano.libraryapi.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
+}
---
+++ libraryapi/src/main/java/com/sipriano/libraryapi/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.sipriano.libraryapi.repository;
+
+import com.sipriano.libraryapi.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LivroRepository extends JpaRepository<Livro, UUID> {
+}
