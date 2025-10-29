07a0c5c9a12a01b6996d2ebfb507523a4e3fc00e
Anderson Sipriano <adsspns@gmail.com>
2025-10-29 18:24:42 -0300

    Iniciando a codificação da API

---
+++ libraryapi/src/main/java/com/sipriano/libraryapi/controller/AutorController.java
@@ -0,0 +1,11 @@
+package com.sipriano.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/autores")
+//host::8080/autores
+//http://localhost/8080/autores
+public class AutorController {
+}
