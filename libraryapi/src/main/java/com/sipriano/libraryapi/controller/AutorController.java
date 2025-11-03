eab0d15a245798316a96746a2cbe8c42cc7e2d1a
Anderson Sipriano <adsspns@gmail.com>
2025-11-03 14:46:34 -0300

    Pesquisa de Autores com filtro

--- libraryapi/src/main/java/com/sipriano/libraryapi/controller/AutorController.java
+++ libraryapi/src/main/java/com/sipriano/libraryapi/controller/AutorController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/autores")
@@ -66,6 +68,22 @@ public class AutorController {
         }
         autorService.deletar(autorOptional.get());
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "nacionalidade", required = false) String nacionalidade) {
+        List<Autor> lista = autorService.pesquisar(nome, nacionalidade);
+        List<AutorDTO> listaDTO = lista
+                .stream()
+                .map(autor -> new AutorDTO(
+                        autor.getId(),
+                        autor.getNome(),
+                        autor.getDataNascimento(),
+                        autor.getNacionalidade()))
+                .toList(); //collect(Collectors.toList());
+        return ResponseEntity.ok(listaDTO);
     }
 
 
--- libraryapi/src/main/java/com/sipriano/libraryapi/service/AutorService.java
+++ libraryapi/src/main/java/com/sipriano/libraryapi/service/AutorService.java
@@ -4,6 +4,7 @@ import com.sipriano.libraryapi.model.Autor;
 import com.sipriano.libraryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,5 +27,21 @@ public class AutorService {
 
     public void deletar(Autor autor) {
         autorRepository.delete(autor);
+    }
+
+    public List<Autor> pesquisar(String nome, String nacionalidade) {
+        if (nome != null && nacionalidade != null) {
+            return autorRepository.findByNomeAndNacionalidade(nome, nacionalidade);
+        }
+
+        if (nome != null) {
+            return autorRepository.findByNome(nome);
+        }
+
+        if (nacionalidade != null){
+            return autorRepository.findByNacionalidade(nacionalidade);
+        }
+
+        return autorRepository.findAll();
     }
 }
