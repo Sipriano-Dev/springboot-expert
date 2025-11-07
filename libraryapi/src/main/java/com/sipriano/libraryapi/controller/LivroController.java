b26dfaf01e1f45ef87023cc792e5b0b2b9cf15f3
Anderson Sipriano <adsspns@gmail.com>
2025-11-07 14:39:57 -0300

    Endpoint para cadastro de livro com Bean Validation

--- libraryapi/src/main/java/com/sipriano/libraryapi/controller/LivroController.java
+++ libraryapi/src/main/java/com/sipriano/libraryapi/controller/LivroController.java
@@ -1,15 +1,37 @@
 package com.sipriano.libraryapi.controller;
 
+import com.sipriano.libraryapi.controller.dto.CadastroLivroDTO;
+import com.sipriano.libraryapi.controller.dto.ErroResposta;
+import com.sipriano.libraryapi.exceptions.RegistroDuplicadoException;
+import com.sipriano.libraryapi.model.Livro;
 import com.sipriano.libraryapi.service.LivroService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/livros")
 @RequiredArgsConstructor
 public class LivroController {
 
     private final LivroService service;
+
+    @PostMapping
+    public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO livroDTO) {
+        try {
+            // mapear dto para entidade
+            // enviar a entidade para o service validar e salvar na base
+            // criar url pra acesso dos dados do livro
+            // retorna codigo created com header location no ResponseEntity
+            return ResponseEntity.ok(livroDTO);
+        } catch (RegistroDuplicadoException e) {
+            var erroDTO = ErroResposta.conflito(e.getMessage());
+            return ResponseEntity.status(erroDTO.status()).body(erroDTO);
+        }
+    }
 
 }
--- libraryapi/src/main/java/com/sipriano/libraryapi/controller/dto/CadastroLivroDTO.java
+++ libraryapi/src/main/java/com/sipriano/libraryapi/controller/dto/CadastroLivroDTO.java
@@ -1,17 +1,38 @@
 package com.sipriano.libraryapi.controller.dto;
 
 import com.sipriano.libraryapi.model.GeneroLivro;
+import com.sipriano.libraryapi.model.Livro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record CadastroLivroDTO(
+        @ISBN
+        @NotBlank(message = "campo obrigatorio")
         String isbn,
+        @NotBlank(message = "campo obrigatorio")
         String titulo,
+        @NotNull(message = "campo obrigatorio")
+        @Past(message = "nao pode ser uma data futura")
         LocalDate dataPublicacao,
         GeneroLivro genero,
         BigDecimal preco,
+        @NotNull(message = "campo obrigatorio")
         UUID idAutor
 ) {
+    public Livro mapearParaLivro() {
+        Livro livro = new Livro();
+        livro.setIsbn(this.isbn);
+        livro.setTitulo(this.titulo);
+        livro.setDataPublicacao(this.dataPublicacao);
+        livro.setGenero(this.genero);
+        livro.setPreco(this.preco);
+
+        return livro;
+    }
 }
