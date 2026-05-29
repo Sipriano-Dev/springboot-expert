c0f868c7911a5cf03a6e1ae3522998557296d2bc
Anderson Sipriano <adsspns@gmail.com>
2026-05-28 22:18:11 -0300

    Configurações globais para os clients

--- libraryapi/src/main/java/com/sipriano/libraryapi/security/CustomRegisteredClientRepository.java
+++ libraryapi/src/main/java/com/sipriano/libraryapi/security/CustomRegisteredClientRepository.java
@@ -6,6 +6,8 @@ import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -15,6 +17,8 @@ import java.util.UUID;
 public class CustomRegisteredClientRepository implements RegisteredClientRepository {
 
     private final ClientService clientService;
+    private final TokenSettings tokenSettings;
+    private final ClientSettings clientSettings;
 
     @Override
     public void save(RegisteredClient registeredClient) {}
@@ -42,6 +46,8 @@ public class CustomRegisteredClientRepository implements RegisteredClientReposit
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .tokenSettings(tokenSettings)
+                .clientSettings(clientSettings)
                 .build();
     }
 }
